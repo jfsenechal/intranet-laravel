ac21787501d263117946469e6a37084642226dd4
jfsenechal <jfsenechal@gmail.com>
2025-11-26 11:19:20 +0100

    add panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,7 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     AcMarche\News\Providers\NewsPanelProvider::class,
     AcMarche\Document\Providers\DocumentPanelProvider::class,
+    AcMarche\Mileage\Providers\MileagePanelProvider::class,
     AcMarche\Security\Providers\SecurityPanelProvider::class,
     AcMarche\Publication\Providers\PublicationPanelProvider::class,
     App\Providers\VoltServiceProvider::class,
---
+++ modules/Mileage/src/Providers/MileagePanelProvider.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace AcMarche\Mileage\Providers;
+
+use AcMarche\App\Traits\PluginTrait;
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+final class MileagePanelProvider extends PanelProvider
+{
+    use PluginTrait;
+
+    public function panel(Panel $panel): Panel
+    {
+        $path = $this->getPluginBasePath().'/../';
+
+        return $panel
+            ->id('mileage-panel')
+            ->path('mileage')
+            ->brandName('Frais de déplacement')
+            ->colors([
+                'primary' => Color::Pink,
+            ])
+            ->unsavedChangesAlerts()
+            ->sidebarCollapsibleOnDesktop()
+            ->discoverResources(in: $path.'Filament/Resources', for: 'AcMarche\\Mileage\\Filament\\Resources')
+            ->discoverPages(in: $path.'Filament/Pages', for: 'AcMarche\\Mileage\\Filament\\Pages')
+            ->pages([
+
+            ])
+            ->discoverWidgets(in: $path.'Filament/Widgets', for: 'AcMarche\\Mileage\\Filament\\Widgets')
+            ->widgets([
+
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+            ]);
+    }
+}
