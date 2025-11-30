cb928fd5a17085396964853ca92f5e556c26f90b
jfsenechal <jfsenechal@gmail.com>
2025-11-30 13:08:06 +0100

    update

--- app/Filament/Admin/Pages/Login.php
+++ app/Filament/Admin/Pages/Login.php
@@ -4,14 +4,14 @@ namespace App\Filament\Admin\Pages;
 
 use Filament\Auth\Pages\Login as BasePage;
 
-class Login extends BasePage
+final class Login extends BasePage
 {
     public function mount(): void
     {
         parent::mount();
-        if (!app()->environment('production')) {
+        if (! app()->environment('production')) {
             $this->form->fill([
-                'email' => 'jf@marche.be',
+                'email' => config('mail.webmaster_email'),
                 'password' => 'marge',
                 'remember' => true,
             ]);
