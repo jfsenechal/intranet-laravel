ef0802d7bee5e1bc7e8c633f17184f7710e553a2
jfsenechal <jfsenechal@gmail.com>
2025-12-03 13:38:22 +0100

    up

--- modules/Publication/src/Filament/Resources/PublicationResource/Pages/ViewPublication.php
+++ modules/Publication/src/Filament/Resources/PublicationResource/Pages/ViewPublication.php
@@ -3,25 +3,32 @@
 namespace AcMarche\Publication\Filament\Resources\PublicationResource\Pages;
 
 use AcMarche\Publication\Filament\Resources\PublicationResource;
+use AcMarche\Publication\Models\Publication;
 use Filament\Actions;
 use Filament\Resources\Pages\ViewRecord;
+use Filament\Support\Colors\Color;
 
 final class ViewPublication extends ViewRecord
 {
     protected static string $resource = PublicationResource::class;
 
+    public function getTitle(): string
+    {
+        return $this->record->name;
+    }
+
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('preview')
+                ->label('Voir la publication')
+                ->icon('tabler-eye')
+                ->color(Color::Green)
+                ->url(fn (Publication $publication) => $publication->url, true),
             Actions\EditAction::make()
                 ->icon('tabler-edit'),
             Actions\DeleteAction::make()
                 ->icon('tabler-trash'),
         ];
-    }
-
-    public function getTitle(): string
-    {
-        return $this->record->name;
     }
 }
