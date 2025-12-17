511688e0f527b81a79f94a49b22a846e75d2a819
jfsenechal <jfsenechal@gmail.com>
2025-12-17 14:35:48 +0100

    reorder imports in `Trip.php` model

--- modules/Mileage/src/Models/Trip.php
+++ modules/Mileage/src/Models/Trip.php
@@ -6,11 +6,11 @@ use AcMarche\Mileage\Observers\TripObserver;
 use AcMarche\Security\Models\HasUserAdd;
 use App\Models\User;
 use Database\Factories\TripFactory;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
 #[UseFactory(TripFactory::class)]
 #[ObservedBy([TripObserver::class])]
