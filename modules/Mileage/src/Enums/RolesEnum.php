65abe81eae81009f9a22cd7a509f8d3e0153f0e3
jfsenechal <jfsenechal@gmail.com>
2025-12-17 17:00:45 +0100

    update `RolesEnum`: implement `getLabel()` and refactor `getRoles()` method

--- modules/Mileage/src/Enums/RolesEnum.php
+++ modules/Mileage/src/Enums/RolesEnum.php
@@ -2,7 +2,6 @@
 
 namespace AcMarche\Mileage\Enums;
 
-use Exception;
 use Filament\Support\Contracts\HasLabel;
 use Illuminate\Contracts\Support\Htmlable;
 
@@ -12,17 +11,25 @@ enum RolesEnum: string implements HasLabel
     case ROLE_FINANCE_DEPLACEMENT_VILLE = 'ROLE_FINANCE_DEPLACEMENT_VILLE';
     case ROLE_FINANCE_DEPLACEMENT_CPAS = 'ROLE_FINANCE_DEPLACEMENT_CPAS';
 
+    /**
+     * @return array<string, string>
+     */
     public static function getRoles(): array
     {
-        return array_values(self::cases());
+        $roles = [];
+        foreach (self::cases() as $role) {
+            $roles[$role->value] = $role->value;
+        }
+
+        return $roles;
     }
 
     public function getLabel(): string|Htmlable|null
     {
         return match ($this) {
-            self::ROLE_FINANCE_DEPLACEMENT_ADMIN => throw new Exception('To be implemented'),
-            self::ROLE_FINANCE_DEPLACEMENT_VILLE => throw new Exception('To be implemented'),
-            self::ROLE_FINANCE_DEPLACEMENT_CPAS => throw new Exception('To be implemented'),
+            self::ROLE_FINANCE_DEPLACEMENT_ADMIN => 'Admin deplacements',
+            self::ROLE_FINANCE_DEPLACEMENT_VILLE => 'Access deplacements ville',
+            self::ROLE_FINANCE_DEPLACEMENT_CPAS => 'Access deplacements CPAS',
             default => null
         };
     }
