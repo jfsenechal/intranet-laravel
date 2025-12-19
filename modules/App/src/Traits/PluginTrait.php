caef476f367717a4ebb6c9f5ba1a27b53333d415
jfsenechal <jfsenechal@gmail.com>
2025-12-19 11:03:56 +0100

    remove unused Heroicon import in PluginTrait

--- modules/App/src/Traits/PluginTrait.php
+++ modules/App/src/Traits/PluginTrait.php
@@ -2,7 +2,6 @@
 
 namespace AcMarche\App\Traits;
 
-use Filament\Support\Icons\Heroicon;
 use ReflectionClass;
 
 trait PluginTrait
@@ -10,7 +9,7 @@ trait PluginTrait
     public function getPluginBasePath($path = null): string
     {
         $reflector = new ReflectionClass(get_class($this));
-Heroicon::QueueList;
+
         return dirname($reflector->getFileName()).($path ?? '');
     }
 }
