ba539d8274951f03f04dfdd810da893e2f3e81f2
Senechal jf <jfsenechal@gmail.com>
2026-05-04 15:41:11 +0200

    up

--- modules/AldermenAgenda/config/database.php
+++ modules/AldermenAgenda/config/database.php
@@ -9,13 +9,13 @@ return [
 
     'connections' => [
         'maria-aldermen-agenda' => [
-            'driver' => env('DB_AGENDA_ECHEVIN_DRIVER', 'mariadb'),
-            'url' => env('DB_AGENDA_ECHEVIN_URL'),
-            'host' => env('DB_AGENDA_ECHEVIN_HOST', env('DB_HOST', '127.0.0.1')),
-            'port' => env('DB_AGENDA_ECHEVIN_PORT', env('DB_PORT', '3306')),
-            'database' => env('DB_AGENDA_ECHEVIN_DATABASE', 'intranet_agenda_echevin'),
-            'username' => env('DB_AGENDA_ECHEVIN_USERNAME', env('DB_USERNAME', 'root')),
-            'password' => env('DB_AGENDA_ECHEVIN_PASSWORD', env('DB_PASSWORD', '')),
+            'driver' => env('DB_ALDERMEN_AGENDA_DRIVER', 'mariadb'),
+            'url' => env('DB_ALDERMEN_AGENDA_URL'),
+            'host' => env('DB_ALDERMEN_AGENDA_HOST', env('DB_HOST', '127.0.0.1')),
+            'port' => env('DB_ALDERMEN_AGENDA_PORT', env('DB_PORT', '3306')),
+            'database' => env('DB_ALDERMEN_AGENDA_DATABASE', 'intranet_agenda_echevin'),
+            'username' => env('DB_ALDERMEN_AGENDA_USERNAME', env('DB_USERNAME', 'root')),
+            'password' => env('DB_ALDERMEN_AGENDA_PASSWORD', env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
