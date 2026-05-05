4b3818fc99ce371ed45de8f2ba23b603c62d56fb
Senechal jf <jfsenechal@gmail.com>
2026-05-05 14:44:33 +0200

    up

--- modules/Offenses/src/Filament/Resources/Offenders/Schemas/OffenderInfolist.php
+++ modules/Offenses/src/Filament/Resources/Offenders/Schemas/OffenderInfolist.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace AcMarche\Offenses\Filament\Resources\Offenses\Schemas;
 
+use AcMarche\Offenses\Filament\Resources\Offenses\OffenseResource;
+use Filament\Infolists\Components\IconEntry;
+use Filament\Infolists\Components\RepeatableEntry;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Model;
 
 final class OffenderInfolist
 {
@@ -14,7 +18,7 @@ final class OffenderInfolist
     {
         return $schema
             ->schema([
-                Section::make('Contrevenant')
+                Section::make('Coordonnées')
                     ->columns(2)
                     ->schema([
                         TextEntry::make('offender.birth_date')
@@ -32,6 +36,30 @@ final class OffenderInfolist
                         TextEntry::make('offender.city')
                             ->label('Localité')
                             ->placeholder('—'),
+                    ]),
+                Section::make('Incivilités')
+                    ->schema([
+                        RepeatableEntry::make('offenses')
+                            ->schema([
+                                TextEntry::make('offenseAct.name')
+                                    ->label('Acte')
+                                    ->url(
+                                        fn(Model $record): string => OffenseResource::getUrl(
+                                            'view',
+                                            ['record' => $record->id]
+                                        )
+                                    ),
+                                TextEntry::make('fine_amount')
+                                    ->label('Amende')
+                                    ->money('EUR')
+                                    ->placeholder('—'),
+                                IconEntry::make('mediation'),
+                                TextEntry::make('decision_date')
+                                    ->label('Date de décision')
+                                    ->date('d/m/Y'),
+                            ])
+                            ->columns(2)
+                            ->grid(2),
                     ]),
             ]);
     }
