30e04408a4250aaf9134f08e826a338150572343
Senechal jf <jfsenechal@gmail.com>
2026-05-12 16:40:34 +0200

    up

---
+++ modules/Telecommunication/src/Providers/TelecommunicationServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AcMarche\Telecommunication\Providers;
+
+use AcMarche\App\Traits\ModuleServiceProviderTrait;
+use Illuminate\Support\ServiceProvider;
+
+final class TelecommunicationServiceProvider extends ServiceProvider
+{
+    use ModuleServiceProviderTrait;
+
+    public static int $module_id = 42;
+
+    public function register(): void
+    {
+        $this->registerModuleConfig();
+    }
+
+    public function boot(): void
+    {
+        // Register commands
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+
+            ]);
+        }
+        $this->bootModule();
+    }
+
+    protected function moduleName(): string
+    {
+        return 'who-is-who';
+    }
+}
