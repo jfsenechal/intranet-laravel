6c945d0fa900f341c95083cdc2a0f2415322a6ef
Senechal jf <jfsenechal@gmail.com>
2026-05-19 11:43:01 +0200

    up

--- modules/Agent/src/Filament/Resources/Profiles/Pages/ListProfiles.php
+++ modules/Agent/src/Filament/Resources/Profiles/Pages/ListProfiles.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace AcMarche\Agent\Filament\Resources\Profiles\Pages;
 
 use AcMarche\Agent\Filament\Resources\Profiles\ProfileResource;
-use Filament\Actions\CreateAction;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ListRecords;
 use Override;
 
@@ -20,9 +20,13 @@ final class ListProfiles extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make()
+            Action::make('addProfileInfo')
                 ->label('Ajouter un profil')
-                ->icon('tabler-plus'),
+                ->icon('tabler-plus')
+                ->modalHeading('Ajouter un profil')
+                ->modalDescription('Pour ajouter un nouveau profil, la demande doit être effectuée par les ressources humaines.')
+                ->modalSubmitAction(false)
+                ->modalCancelActionLabel('Fermer'),
         ];
     }
 }
