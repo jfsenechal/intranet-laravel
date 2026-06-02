21b7cb26e504a96d276d13913749df43bb52bf71
Senechal jf <jfsenechal@gmail.com>
2026-06-02 15:48:02 +0200

    up

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,8 @@ final class AdminPanelProvider extends PanelProvider
             ])
             ->databaseNotifications()
             ->unsavedChangesAlerts()
+            ->resourceCreatePageRedirect('view')
+            ->resourceEditPageRedirect('view')
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
