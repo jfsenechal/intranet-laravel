746a1fbdb929eb2a8038cec71a2040bf4c8775e6
jfsenechal <jfsenechal@gmail.com>
2026-07-01 15:37:28 +0200

    add

---
+++ tests/Browser/Filament/Resources/IncomingMailFormPreviewTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use AcMarche\Courrier\Enums\RolesEnum;
+use AcMarche\Security\Models\Role;
+use App\Models\User;
+
+it('shows the incoming mail create form with preview on the left', function (): void {
+    $user = User::factory()->create([
+        'email' => 'courrier-demo@pestphp.com',
+        'password' => 'password',
+    ]);
+    $role = Role::factory()->create(['name' => RolesEnum::ROLE_INDICATEUR_VILLE_ADMIN->value]);
+    $user->roles()->attach($role);
+
+    $this->actingAs($user);
+
+    visit(route('filament.courrier-panel.resources.incoming-mails.create'))
+        ->assertSee('Aperçu')
+        ->assertSee('Informations du courrier')
+        ->screenshot('incoming-mail-create-form');
+});
