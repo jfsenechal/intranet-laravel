8e8fc69873d908a00e292cf2a8089a2ec1e820c1
Senechal jf <jfsenechal@gmail.com>
2026-07-07 10:48:40 +0200

    up

--- modules/CpasLibrary/src/Filament/Resources/Categories/CategorieResource.php
+++ modules/CpasLibrary/src/Filament/Resources/Categories/CategorieResource.php
@@ -33,7 +33,7 @@ final class CategorieResource extends Resource
     protected static string|UnitEnum|null $navigationGroup = 'Bibliothèque';
 
     #[Override]
-    protected static ?int $navigationSort = 1;
+    protected static ?int $navigationSort = 2;
 
     #[Override]
     protected static ?string $navigationLabel = 'Catégories';
--- modules/CpasLibrary/src/Filament/Resources/Fiches/FicheResource.php
+++ modules/CpasLibrary/src/Filament/Resources/Fiches/FicheResource.php
@@ -31,7 +31,7 @@ final class FicheResource extends Resource
     protected static string|UnitEnum|null $navigationGroup = 'Bibliothèque';
 
     #[Override]
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 1;
 
     #[Override]
     protected static ?string $navigationLabel = 'Fiches';
