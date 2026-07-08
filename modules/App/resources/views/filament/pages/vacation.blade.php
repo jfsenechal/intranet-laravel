f5e3f94bc2412bf513c008e1523d2216036b7486
jfsenechal <jfsenechal@gmail.com>
2026-07-08 11:46:03 +0200

    up

--- modules/App/resources/views/filament/pages/vacation.blade.php
+++ modules/App/resources/views/filament/pages/vacation.blade.php
@@ -8,13 +8,14 @@
         <x-slot name="heading">
             Message d'absence pour ma messagerie mail
         </x-slot>
-        Votre message d'absence peut être activé via :<br/>
-        <a href="https://agenda.marche.be/SOGo/so/{{$user->username}}/Preferences#!/mailer" target="_blank">https://agenda.marche.be</a>
-        <br/><br/> Cliquez sur "Absence prolongée"
- <br/><br/>
-        <a href="https://formation.marche.be/?p=581" target="_blank">
-            Vous pouvez trouver de l'aide sur notre site Formation.marche.be
+        Votre message d'absence doit être activé via <a
+            href="https://agenda.marche.be/SOGo/so/{{$user->username}}/Preferences#!/mailer" target="_blank">
+            https://agenda.marche.be.
         </a>
+        <br/><br/>
+        Cliquez sur <strong>« Absence prolongée »</strong> pour configurer votre message d'absence.
+        <br/><br/>
+        Une fiche explicative est également disponible sur notre site : <a href="https://formation.marche.be/?p=581" target="_blank">https://formation.marche.be</a>.
 
     </x-filament::section>
 
